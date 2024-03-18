d9099483a3822d7ba217222c93345fe36348714e
Tursunov Imran <tursunov.imran@mail.ru>
2024-03-18 15:29:32 +0300

    Added resolves for test tasks

---
+++ cmd/task_5/task_5.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type SomeStruct struct {
+	ID int 
+	Numbers []int
+}
+
+func mergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left, right []int) []int {
+	result := make([]int, 0)
+	l, r := 0, 0
+
+	for l < len(left) && r < len(right) {
+		if left[l] <= right[r] {
+			result = append(result, left[l])
+			l++
+		} else {
+			result = append(result, right[r])
+			r++
+		}
+	}
+
+	result = append(result, left[l:]...)
+	result = append(result, right[r:]...)
+
+	return result
+}
+
+
+func main() {
+	arr := []SomeStruct{
+		{ID: 1, Numbers: []int{4, 2, 9, 1, 5}},
+		{ID: 2, Numbers: []int{7, 3, 8, 6}},
+		{ID: 3, Numbers: []int{10, 15, 12}},
+	}
+
+	for i, v := range(arr) {
+		arr[i].Numbers = mergeSort(v.Numbers)
+	}
+	fmt.Println(arr)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/TursunovImran/wildberies_backend_tasks
+
+go 1.21.6
